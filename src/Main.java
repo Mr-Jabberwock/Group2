b97133e86e3780fab2340bae765f4a982f0d3060
Danny Johnsen <djohnsen143@gmail.com>
2017-09-15 10:03:12 +0200

    Gadit

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,12 @@
+import com.sun.org.apache.xpath.internal.SourceTree;
+
 public class Main {
 
     public static void main(String[] args) {
 
         System.out.println("Hello World!");
         System.out.println("Waddup, World");
+        int fuckit = 0;
 
         //hej
     }
